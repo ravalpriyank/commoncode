accc016defaf18e872710f473d06c04da374b864
Raval Tejas <ravaltejash2004@gmail.com>
2025-06-24 02:37:10 -0700

    all are cleared

--- file1.php
+++ file1.php
@@ -1,18 +1,3 @@
 <?php 
     echo "Welcome to file1";
-
-    for($i=1;$i<=10;$i++)
-    {
-        echo $i."<br>";
-    }
-
-    for($j=11;$j<=20;$j++)
-    {
-        echo $j."<br>";
-    }
-
-    for($l=31;$l<=40;$l++)
-    {
-        echo $l."<br>";
-    }
 ?>
